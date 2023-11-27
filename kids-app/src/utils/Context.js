e70d9bda21a8f06c65be0ddd20060b15ebb34085
ALTCODE <alteevee25@gmail.com>
2023-11-27 09:49:17 -0800

    fixing token

--- kids-app/src/utils/Context.js
+++ kids-app/src/utils/Context.js
@@ -102,28 +102,11 @@ const AppProvider = ({ children }) => {
 
   const [cookies, setCookies] = useCookies(["login_token"]);
 
-  const cookiess = Cookies.get("login_token");
+  const cookiess = Cookies.get("login_token", {
+    sameSite: "none",
+  });
   // console.log({ cookies, cookiess });
   console.log({ cookiess, cookiess });
-
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    console.log(decodedCookie);
-    let ca = decodedCookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == " ") {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-
-  getCookie("login_token");
 
   //*****************************USE EFFECTS ************************** */
   useEffect(() => {
