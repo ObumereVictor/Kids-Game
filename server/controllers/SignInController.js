ae6c0fcbd21f754a35918065640b9e6f31d93762
ALTCODE <alteevee25@gmail.com>
2023-11-26 16:50:12 -0800

    token new commit

--- server/controllers/SignInController.js
+++ server/controllers/SignInController.js
@@ -44,7 +44,7 @@ const signIn = async (request, response, next) => {
   });
 
   response.cookie("login_token", token, {
-    httpOnly: false,
+    HttpOnly: false,
     maxAge: 60 * 60 * 24,
     secure: true,
     sameSite: "none",
