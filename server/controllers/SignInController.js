d863777648f185cb0d1880f3d0b1179407ad35cc
ALTCODE <alteevee25@gmail.com>
2023-11-28 07:27:34 -0800

    new commit

--- server/controllers/SignInController.js
+++ server/controllers/SignInController.js
@@ -45,10 +45,9 @@ const signIn = async (request, response, next) => {
 
   response.cookie("login_token", token, {
     httpOnly: false,
-    // path: "https://kids-game.onrender.com/",
     maxAge: 60 * 60 * 24,
     secure: true,
-    domain: "kids-spelling-game.onrender.com",
+    // domain: "kids-spelling-game.onrender.com",
     sameSite: "none",
   });
   // IS USER VERIFIED
