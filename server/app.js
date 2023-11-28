3f8bece5093ff029ac254a1e2a7c9696f1a087cc
ALTCODE <alteevee25@gmail.com>
2023-11-28 11:10:09 -0800

    new commit

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: originLink,
+    origin: true,
     credentials: true,
     optionsSuccessStatus: 200,
   })
