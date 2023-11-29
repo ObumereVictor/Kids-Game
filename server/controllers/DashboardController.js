5014607d6efcf96fd77c5cb2dbc17d53b9cc3e16
ALTCODE <alteevee25@gmail.com>
2023-11-29 01:19:53 -0800

    new commit

--- server/controllers/DashboardController.js
+++ server/controllers/DashboardController.js
@@ -91,7 +91,7 @@ const completeProfile = async (request, response) => {
 };
 
 const dashboard = async (request, response) => {
-  console.log(request.headers.cookie);
+  console.log(request.headers);
   if (!request.headers.cookie) {
     return;
   }
