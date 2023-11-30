4a1e7040bea405b065bdff97b751ded1056bdf65
ALTCODE <alteevee25@gmail.com>
2023-11-30 01:46:27 -0800

    refresh token

--- server/controllers/DashboardController.js
+++ server/controllers/DashboardController.js
@@ -92,6 +92,7 @@ const completeProfile = async (request, response) => {
 
 const dashboard = async (request, response) => {
   console.log(request.headers);
+  console.log(request.cookies);
   if (!request.headers.cookie) {
     return response.status(StatusCodes.BAD_REQUEST).json({
       status: "Failed",
