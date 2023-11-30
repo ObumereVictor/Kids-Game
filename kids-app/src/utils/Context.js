2907c8b2c0e4bc56f8c42bb1ed2d9d93c1195ee4
ALTCODE <alteevee25@gmail.com>
2023-11-30 02:03:08 -0800

    new commit

--- kids-app/src/utils/Context.js
+++ kids-app/src/utils/Context.js
@@ -152,6 +152,7 @@ const AppProvider = ({ children }) => {
 
         setGameDetails({ ...response.data });
       } catch (error) {
+        console.log(error);
         throw new Error("Sign in");
         setAuthenticated({
           user: true,
