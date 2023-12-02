c0536851d1228d4341ff9926728ed149daded1df
ALTCODE <alteevee25@gmail.com>
2023-12-02 14:58:50 -0800

    new commit

--- kids-app/src/pages/Routes/ProtectedRoute.js
+++ kids-app/src/pages/Routes/ProtectedRoute.js
@@ -52,7 +52,7 @@ const EditProfileProtectRoute = ({ token, cookie, children }) => {
 
 const GameProtectedRoute = ({ token, gameId, cookie, children }) => {
   console.log({ token, cookie, gameId });
-  if ((token || cookie) && gameId) return children;
+  if (token || cookie) return children;
   return <Navigate to="../../sign-in" replace="true" />;
 };
 
