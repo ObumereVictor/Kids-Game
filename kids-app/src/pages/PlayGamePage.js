ac5328eb7723a35183e8225511c7176919e4592e
ALTCODE <alteevee25@gmail.com>
2023-12-14 00:31:14 -0800

    new commit

--- kids-app/src/pages/PlayGamePage.js
+++ kids-app/src/pages/PlayGamePage.js
@@ -198,7 +198,6 @@ const PlayGamePage = () => {
           {game.map((spelling, index) => {
             // console.log(spelling.gid, index);
             // let { gid, game } = spelling;
-            console.log([...spelling.game]);
             return (
               <button
                 data-gid={index}
