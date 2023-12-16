9833c2a6a5976adea50460008c24b648a9cd544f
ALTCODE <alteevee25@gmail.com>
2023-12-16 08:04:48 -0800

    new commit

--- server/controllers/GameController.js
+++ server/controllers/GameController.js
@@ -37,7 +37,7 @@ const createGame = async (request, response) => {
     return game.game;
   });
 
-  console.log(isGameAvaliable);
+  console.log(isGameAvaliable[0]);
 
   return;
   if (isGameAvaliable) {
