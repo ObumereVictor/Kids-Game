5b5ca8ceda6db6ca0cb34dd0a77312b357c61da5
ALTCODE <alteevee25@gmail.com>
2023-12-15 23:13:49 -0800

    new commit

--- server/controllers/GameController.js
+++ server/controllers/GameController.js
@@ -33,10 +33,11 @@ const createGame = async (request, response) => {
   let gameArray = [];
   let isGameAvaliable = await GameSchema.find({});
   isGameAvaliable = isGameAvaliable.map((game) => {
+    console.log(game);
     return game.game;
   });
 
-  isGameAvaliable = isGameAvaliable.map((game) => game);
+  // isGameAvaliable = isGameAvaliable.map((game) => game);
 
   // isGameAvaliable = gameArray.concat(...isGameAvaliable).join("");
   console.log({ game, isGameAvaliable });
