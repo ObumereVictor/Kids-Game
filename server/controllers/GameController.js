3ea4dff4f5674a8844f05b16dd731b0c91c8d036
ALTCODE <alteevee25@gmail.com>
2023-12-16 09:48:07 -0800

    new commit

--- server/controllers/GameController.js
+++ server/controllers/GameController.js
@@ -38,7 +38,8 @@ const createGame = async (request, response) => {
     return game.game;
   });
 
-  for (let index = 0; index <= isGameAvaliable.lenght; index++) {
+  for (let index = 0; index <= games.lenght; index++) {
+    console.log("F");
     let g = isGameAvaliable[index].game.map((words) => {
       // console.log(words.game);
       return words.game;
